398e8d0bcf6a5705bdfd26f8128865afa55987e5
ruxxzebre <54982966+ruxxzebre@users.noreply.github.com>
2020-04-24 12:20:19 +0300

    Added measurements

--- 10lab.py
+++ 10lab.py
@@ -1,5 +1,8 @@
 author = "Pavlo Chaikovskyi 125B"
 import numpy as np
+import time
+import tracemalloc
+import sys
 
 """
 ОСНОВНІ :
@@ -26,6 +29,30 @@ n*n, де 1<=n<=5.
 """
 DECIMAL TO HEXADECIMAL ALGORITHM TAKEN FROM HERE :
 https://www.permadi.com/tutorial/numDecToHex/
+"""
+
+"""def time_check(func):
+    # simple decorator for time measurement
+
+    def wrapper(*args, **kwargs):
+        start = time.time ()
+        return_value = func (*args, **kwargs)
+        end = time.time ()
+        print (f"Execution time : {end - start}")
+        return return_value
+    return wrapper
+
+def memory_check(func):
+    # simple decorator for memory measurement
+
+    def wrapper(*args, **kwargs):
+        tracemalloc.start ()
+        return_value = func(*args, **kwargs)
+        current, peak = tracemalloc.get_traced_memory ()
+        print (f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+        tracemalloc.stop ()
+        return return_value
+    return wrapper
 """
 
 def factorial_iter(n):
@@ -166,6 +193,23 @@ def decToHexadec_recc(num, reminders = []):
         return decToHexadec_recc(num, reminders)
 
 if __name__ == "__main__":
+    """
+    Here we have few variables for the sake of choosing what we need,
+    how to call function (recursively or not), and what function.
+    
+    To avoid side effect stuff, like  - conditions, inputs, dynamic type changing, 
+    i've made function called "func_calling", so it can return us a function we need and
+    value that we get from input. And then we can measure time and memory without it,
+    just clean function calling and passing there an argument.
+    
+    Sure, it could be done with just wrapping every function in every condition ever
+    called, but I'm a bit lazy and do not like to copy code snippets.
+    That's the time, when laziness gives an benefits in time and code beauty (personally 
+    I think it's beautiful, except variable names). I've also had learned a lot by
+    not wanting to copy snippets a hundreds times in my code, it kinda forced me to learn
+    what to do, to avoid that.
+    """
+
     while True:
         b00l = input("Recursively? y/n ")
         what = int(input("Okay\nSo..\nWhat will you choose :\nFactorial (0)\nDigital root (1)\n"
@@ -175,45 +219,83 @@ if __name__ == "__main__":
         if b00l in ('y', 'n') and what in range(0, 5 + 1):
             break
 
-    if what == 0:
-        n = int(input('Enter num : '))
-        if b00l == 'y' :
-            print(factorial_recc(n))
-        elif b00l == 'n' :
-            print(factorial_iter(n))
-    elif what == 1:
-        n = int (input ('Enter num : '))
-        if b00l == 'y':
-            print (digitalroot_recc(n))
-        elif b00l == 'n':
-            print(digitalroot_iter(n))
-    elif what == 2:
-        while True:
-            n = int(input('Enter n : '))
-            if n in range(1, 5 + 1):
-                break
+    def func_calling():
+        global b00l
+        global what
 
-        array = np.random.randint(0, 100, size=(n, n))
-        print(array)
-        if b00l == 'y' :
-            print(maxIndex_recc(array))
-        elif b00l == 'n':
-            print (maxIndex_iter(array))
-    elif what == 3:
-        array = input('Enter few values, separated by coma : \n').split(', ')
-        if b00l == 'y' :
-            print(backwards_recc(array))
-        elif b00l == 'n':
-            print (backwards_iter(array))
-    elif what == 4:
-        n = int(input("Enter num : "))
-        if b00l == 'y' :
-            print(simplenum_recc(n))
-        elif b00l == 'n':
-            print (simplenum_iter(n))
-    elif what == 5:
-        n = int(input("Enter decimal : "))
-        if b00l == 'y' :
-            print(decToHexadec_recc(n))
-        elif b00l == 'n':
-            print (decToHexadec_iter(n))
+        if what == 0:
+            n = int(input('Enter num : '))
+            if b00l == 'y' :
+                return (factorial_recc, n)
+            elif b00l == 'n' :
+                return (factorial_iter, n)
+        elif what == 1:
+            n = int (input ('Enter num : '))
+            if b00l == 'y':
+                return (digitalroot_recc, n)
+            elif b00l == 'n':
+                return (digitalroot_iter, n)
+        elif what == 2:
+            while True:
+                n = int(input('Enter n : '))
+                if n in range(1, 5 + 1):
+                    break
+
+            array = np.random.randint(0, 100, size=(n, n))
+            print(array)
+            if b00l == 'y' :
+                return (maxIndex_recc, array)
+            elif b00l == 'n':
+                return (maxIndex_iter, array)
+        elif what == 3:
+            array = input('Enter few values, separated by gap : \n').split()
+            if b00l == 'y' :
+                return (backwards_recc, array)
+            elif b00l == 'n':
+                return (backwards_iter, array)
+        elif what == 4:
+            n = int(input("Enter num : "))
+            if b00l == 'y' :
+                return (simplenum_recc, n)
+            elif b00l == 'n':
+                return (simplenum_iter, n)
+        elif what == 5:
+            n = int(input("Enter decimal : "))
+            if b00l == 'y' :
+                return (decToHexadec_recc, n)
+            elif b00l == 'n':
+                return (decToHexadec_iter, n)
+
+    """
+    Here we have measurements implementation.
+    
+    So, func, value it's just function we wanted and variable we want to pass in.
+    func_calling() returned us an tuple of these.
+    
+    "start" variable begins a "timer"
+    "tracemalloc.start()" begins tracing memory
+    
+    and after functions done everything we want
+    we have traced memory, and ending time.
+    """
+
+    func, value = func_calling()
+
+    start = time.time ()
+    tracemalloc.start ()
+
+    result = func(value)
+
+    current, peak = tracemalloc.get_traced_memory ()
+    end = time.time ()
+
+    print (f"Execution time : {end - start}")
+    print (f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+
+    tracemalloc.stop ()
+
+    if type(result) == list:
+        print(*result, sep=", ", end="")
+        print('.')
+    else:
+        print(result)
